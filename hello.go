de6a83700166d3a767965a7f35dbc8e782064168
Hayahito Kawamitsu <halation3@gmail.com>
2017-03-31 11:11:24 +0900

    fix

--- hello.go
+++ hello.go
@@ -3,15 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 	"log"
+	"os/exec"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	hostname, err := os.Hostname()
+	hostname, err := exec.Command("hostname").Output()
 	if err != nil {
-		fmt.Fprintf(w, "Hello, World")
-		fmt.Fprintf(w, hostname)
+		fmt.Fprintf(w, "Hello, World %s", hostname)
 	} else {
 		log.Fatal(err)
 	}
